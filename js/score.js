2214f6cb6fabaf0db8319fbad15899f11df4d667
XDtoMe <josepaule404@gmail.com>
2026-05-24 22:42:59 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,37 +1,19 @@
 /**
  * Numbers of decimal digits to round to
  */
-const scale = 3;
+const scale = 2;
 
 /**
  * Calculate the score awarded when having a certain percentage on a list level
  * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
  * @returns {Number}
  */
-export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+export function score(rank) {
+    if (rank > 100) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
-
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    score = Math.max(0, score);
-
-    if (percent != 100) {
-        return round(score - score / 3);
-    }
+    
+    let score = (500.00*Math.pow(0.95,rank-1));
 
     return Math.max(round(score), 0);
 }
